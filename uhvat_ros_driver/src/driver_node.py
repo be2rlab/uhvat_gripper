40c9cfcf491a232eab9e7a70c49cc630d7315977
kirillin <kaartemov@gmail.com>
2021-07-22 01:09:44 +0300

    Works good with the system

--- uhvat_ros_driver/src/driver_node.py
+++ uhvat_ros_driver/src/driver_node.py
@@ -1,11 +1,15 @@
 #!/usr/bin/env python3
-import os
+import os, time
 import rospy
+import math
 from dynamixel_sdk import *
 from uhvat_festolike_driver.srv import *
 
 from sensor_msgs.msg import JointState
 from std_srvs.srv import Empty
+
+import threading
+lock = threading.Lock()
 
 import sys, tty, termios
 fd = sys.stdin.fileno()
@@ -67,9 +71,12 @@ class GripperDriver(object):
         # rospy.Service('gripper_change_mode', Empty, self.__change_mode)
         
         # state publisher
-        self.pub = rospy.Publisher('/uhvat/joint_states', JointState, queue_size=10)
+        self.pub = rospy.Publisher('/joint_states', JointState, queue_size=10)
 
         self.mode = 'position'
+        self.left_pos, self.right_pos = 0, 0
+        self.prev_left_pos, self.prev_right_pos = 0, 0
+        self.driver_state = 'ready'  # `read` or `write` or `ready`
 
     def init(self):
         """
@@ -133,44 +140,51 @@ class GripperDriver(object):
 
 
     def __get_gripper_state(self):
-        
         l_dxl_present_position, l_dxl_comm_result, l_dxl_error = packetHandler.read4ByteTxRx(portHandler, LEFT_DXL_ID, ADDR_PRESENT_POSITION)
         r_dxl_present_position, r_dxl_comm_result, r_dxl_error = packetHandler.read4ByteTxRx(portHandler, RIGHT_DXL_ID, ADDR_PRESENT_POSITION)
-
-        # print("Present Position of ID %s = %s" % (req.id, dxl_present_position))
         return l_dxl_present_position, r_dxl_present_position
 
     def __set_gripper_state(self, req):
-        if req.state == 0:   # wide opened
-            self.__switch('position')
-            dxl_comm_result, dxl_error = packetHandler.write4ByteTxRx(portHandler, LEFT_DXL_ID, ADDR_GOAL_POSITION, LEFT_OPENED_POSITION)
-            dxl_comm_result, dxl_error = packetHandler.write4ByteTxRx(portHandler, RIGHT_DXL_ID, ADDR_GOAL_POSITION, RIGHT_OPENED_POSITION)
-        elif req.state == 1: # less wide opened
-            self.__switch('position')
-            dxl_comm_result, dxl_error = packetHandler.write4ByteTxRx(portHandler, LEFT_DXL_ID, ADDR_GOAL_POSITION, LEFT_OPENED_POSITION + LESS_WIDE_OPENED)
-            dxl_comm_result, dxl_error = packetHandler.write4ByteTxRx(portHandler, RIGHT_DXL_ID, ADDR_GOAL_POSITION, RIGHT_OPENED_POSITION - LESS_WIDE_OPENED)
-        elif req.state == 2: # before position of full closed
-            self.__switch('position')
-            dxl_comm_result, dxl_error = packetHandler.write4ByteTxRx(portHandler, LEFT_DXL_ID, ADDR_GOAL_POSITION, LEFT_CLOSED_POSITION)
-            dxl_comm_result, dxl_error = packetHandler.write4ByteTxRx(portHandler, RIGHT_DXL_ID, ADDR_GOAL_POSITION, RIGHT_CLOSED_POSITION)
-        elif req.state == 3: # full closed
-            self.__switch('position')
-            dxl_comm_result, dxl_error = packetHandler.write4ByteTxRx(portHandler, LEFT_DXL_ID, ADDR_GOAL_POSITION, LEFT_CLOSED_POSITION + FULL_CLOSED)
-            dxl_comm_result, dxl_error = packetHandler.write4ByteTxRx(portHandler, RIGHT_DXL_ID, ADDR_GOAL_POSITION, RIGHT_CLOSED_POSITION - FULL_CLOSED)
-        elif req.state == 4: # speed -- low force
-            self.__switch('speed')
-            dxl_comm_result, dxl_error = packetHandler.write2ByteTxRx(portHandler, LEFT_DXL_ID, 32, 150)
-            dxl_comm_result, dxl_error = packetHandler.write2ByteTxRx(portHandler, RIGHT_DXL_ID, 32, 1023 + 150)
-        elif req.state == 5: # speed -- more force
-            self.__switch('speed')
-            dxl_comm_result, dxl_error = packetHandler.write2ByteTxRx(portHandler, LEFT_DXL_ID, 32, 300)
-            dxl_comm_result, dxl_error = packetHandler.write2ByteTxRx(portHandler, RIGHT_DXL_ID, 32, 1023 + 300)
-        elif req.state == 6: # speed -- max force
-            self.__switch('speed')
-            dxl_comm_result, dxl_error = packetHandler.write2ByteTxRx(portHandler, LEFT_DXL_ID, 32, 450)
-            dxl_comm_result, dxl_error = packetHandler.write2ByteTxRx(portHandler, RIGHT_DXL_ID, 32, 1023 + 450)
-        else:
-            rospy.logwarn(f"There is no state \"{req.state}\"")
+        lock.acquire()
+        if self.driver_state == 'ready':
+            self.driver_state = 'write'
+            while portHandler.is_using:
+                time.sleep(0.01)
+
+            if req.state == 0:   # wide opened
+                self.__switch('position')
+                dxl_comm_result, dxl_error = packetHandler.write4ByteTxRx(portHandler, LEFT_DXL_ID, ADDR_GOAL_POSITION, LEFT_OPENED_POSITION)
+                dxl_comm_result, dxl_error = packetHandler.write4ByteTxRx(portHandler, RIGHT_DXL_ID, ADDR_GOAL_POSITION, RIGHT_OPENED_POSITION)
+            elif req.state == 1: # less wide opened
+                self.__switch('position')
+                dxl_comm_result, dxl_error = packetHandler.write4ByteTxRx(portHandler, LEFT_DXL_ID, ADDR_GOAL_POSITION, LEFT_OPENED_POSITION + LESS_WIDE_OPENED)
+                dxl_comm_result, dxl_error = packetHandler.write4ByteTxRx(portHandler, RIGHT_DXL_ID, ADDR_GOAL_POSITION, RIGHT_OPENED_POSITION - LESS_WIDE_OPENED)
+            elif req.state == 2: # before position of full closed
+                self.__switch('position')
+                dxl_comm_result, dxl_error = packetHandler.write4ByteTxRx(portHandler, LEFT_DXL_ID, ADDR_GOAL_POSITION, LEFT_CLOSED_POSITION)
+                dxl_comm_result, dxl_error = packetHandler.write4ByteTxRx(portHandler, RIGHT_DXL_ID, ADDR_GOAL_POSITION, RIGHT_CLOSED_POSITION)
+            elif req.state == 3: # full closed
+                self.__switch('position')
+                dxl_comm_result, dxl_error = packetHandler.write4ByteTxRx(portHandler, LEFT_DXL_ID, ADDR_GOAL_POSITION, LEFT_CLOSED_POSITION + FULL_CLOSED)
+                dxl_comm_result, dxl_error = packetHandler.write4ByteTxRx(portHandler, RIGHT_DXL_ID, ADDR_GOAL_POSITION, RIGHT_CLOSED_POSITION - FULL_CLOSED)
+            elif req.state == 4: # speed -- low force
+                self.__switch('speed')
+                dxl_comm_result, dxl_error = packetHandler.write2ByteTxRx(portHandler, LEFT_DXL_ID, 32, 150)
+                dxl_comm_result, dxl_error = packetHandler.write2ByteTxRx(portHandler, RIGHT_DXL_ID, 32, 1023 + 150)
+            elif req.state == 5: # speed -- more force
+                self.__switch('speed')
+                dxl_comm_result, dxl_error = packetHandler.write2ByteTxRx(portHandler, LEFT_DXL_ID, 32, 300)
+                dxl_comm_result, dxl_error = packetHandler.write2ByteTxRx(portHandler, RIGHT_DXL_ID, 32, 1023 + 300)
+            elif req.state == 6: # speed -- max force
+                self.__switch('speed')
+                dxl_comm_result, dxl_error = packetHandler.write2ByteTxRx(portHandler, LEFT_DXL_ID, 32, 450)
+                dxl_comm_result, dxl_error = packetHandler.write2ByteTxRx(portHandler, RIGHT_DXL_ID, 32, 1023 + 450)
+            else:
+                rospy.logwarn(f"There is no state \"{req.state}\"")
+            
+            self.driver_state = 'ready'
+        lock.release()
+        return []
 
     def __reboot(self, req):
         for id in [LEFT_DXL_ID, RIGHT_DXL_ID]:                        
@@ -194,21 +208,62 @@ class GripperDriver(object):
 
 
     def spin(self):
-        rate = rospy.Rate(30)
-        while not rospy.is_shutdown():
-            # dxl_present_position, dxl_comm_result, dxl_error = packetHandler.read4ByteTxRx(portHandler, LEFT_DXL_ID, ADDR_PRESENT_POSITION)
-            # print(dxl_present_position)
-            try:
-                left_pos, right_pos = self.__get_gripper_state()
-                print(left_pos, right_pos)
 
+        """ 
+            The gripper illustration:
+            
+              | |
+              |.|         --- flange
+            \|   |/       --- fingers
+            0     1       --- indexes in urdf
+         left    right    --- view from top direction
+
+            Opened (full) position is [tics]:
+        214 810 --- let's it is a zero position
+            Closed (full) position is [tics]:
+        572 438 --- it is a limit position
+
+        [tics to deg] => 
+            alpha = (pos - zero_position) / 1024
+        
+        
+        """
+        LEFT_ZERO_POSITION = 214 * 300 / 1024
+        RIGHT_ZERO_POSITION = 810 * 300 / 1024
+
+        rate = rospy.Rate(300)
+        while not rospy.is_shutdown():
+
+            if self.driver_state == 'ready':
+                self.driver_state == 'read'
+                if not portHandler.is_using:
+                    l, r = self.__get_gripper_state()
+                    if l + r < 3000:
+                        self.left_pos, self.right_pos = l, r
+                    # print(self.left_pos, self.right_pos)
+                self.driver_state == 'ready'
+
+            # transform [tics] to [rad] with offset
+            left_pos_rad = (LEFT_ZERO_POSITION - self.left_pos * 300 / 1024) * math.pi / 180
+            right_pos_rad = (RIGHT_ZERO_POSITION - self.right_pos * 300 / 1024) * math.pi / 180
+
+            # filtring measurements
+            left_pos_rad = (left_pos_rad + self.prev_left_pos) / 2
+            right_pos_rad = (right_pos_rad + self.prev_right_pos) / 2
+            self.prev_left_pos, self.prev_right_pos = left_pos_rad, right_pos_rad
+
+            # print(left_pos_rad, right_pos_rad)    # debug msg
+
+            try:
                 msg = JointState()
                 msg.header.stamp.secs = rospy.get_rostime().secs
                 msg.header.stamp.nsecs = rospy.get_rostime().nsecs
-                msg.name.append('uhvat_joint_1')
+                
+                # TODO using lists below
+                msg.name.append('uhvat_joint_1')    # left but right on the arm :)
                 msg.name.append('uhvat_joint_2')
-                msg.position.append(1)
-                msg.position.append(-1)
+                msg.position.append(left_pos_rad)  # left
+                msg.position.append(right_pos_rad)
 
                 self.pub.publish(msg)
             except Exception as e:
